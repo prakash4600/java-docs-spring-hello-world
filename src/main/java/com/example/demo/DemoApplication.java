a6e7cd7225aa94643338358ccaac6c684cb60fcd
prakash4600 <114327234+prakash4600@users.noreply.github.com>
2023-02-08 14:16:11 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String sayHello() {
-		return "Hello World!";
+		return "Hello Devops Pilot , Conductor and FinOps Batch This is java aplication From Azure😊";
 	}
 }
